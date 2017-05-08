5968ca9b7d4e28712f525dce4ef2b090a8aabbdd
christian <c-style@live.dk>
2017-05-08 11:27:58 +0200

    chat done

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-  database: 'mongodb://chrion:cstyleeever02@ds133260.mlab.com:33260/meanauth',
+  database: 'mongodb://chrion:1234@ds133260.mlab.com:33260/meanauth',
   secret: 'yoursecret'
 }
